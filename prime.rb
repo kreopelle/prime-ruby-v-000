e0219732f9105f73daa3004d439d2bbe519d3519
Kayla Reopelle <kreopelle@gmail.com>
2018-04-09 11:46:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(my_number)
  if my_number >= 1
   test_array = (2..my_number).to_a
   test_array.each do |test_num|
-    if test_num**(my_number-1) == my_number % test_num
+    if test_num**(my_number-1) % my_number == 0
       true
     else
       false
